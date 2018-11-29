2bc69d5586b1486540ea4fa91b80531a324f524b
Bryant Pham <bryantphamm@gmail.com>
2018-11-29 16:44:51 -0600

    intensity slice gui shell

--- src/app/intensityslice/intensityslice.component.ts
+++ src/app/intensityslice/intensityslice.component.ts
@@ -1,37 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { ImageProcessingService } from '../services/imageprocessing.service';
 import { DIPResponse } from '../models/DIPResponse';
+import { Image } from '../models/image';
+import { SelectedImageService } from '../services/selectedimage.service';
 
 @Component({
   selector: 'intensity-slice',
   styleUrls: [ './intensityslice.component.css' ],
   templateUrl: './intensityslice.component.html'
 })
-export class IntensitySliceComponent {
+export class IntensitySliceComponent implements OnInit {
   public image: File;
   public sliceCounter: string[] = [''];
   public sliceValues: string[] = [''];
-  public intervalCounter: string[] = ['', ''];
-  public intervalColors: string[] = ['', ''];
+  public sliceColors: string[] = [''];
   public loading = false;
+  public result: DIPResponse;
+  public selectedImage: Image;
 
   constructor(
-    public processingService: ImageProcessingService
+    public processingService: ImageProcessingService,
+    private selectedImageService: SelectedImageService
   ) {}
+
+  public ngOnInit(): void {
+    this.selectedImageService.getSelectedImage()
+      .subscribe((image: Image) => this.selectedImage = image);
+  }
 
   public addSlice(): void {
     this.sliceCounter.push('');
     this.sliceValues.push('');
-    this.intervalCounter.push('');
-    this.intervalColors.push('');
+    this.sliceColors.push('');
   }
 
   public removeSlice(): void {
     this.sliceCounter.pop();
     this.sliceValues.pop();
-    this.intervalCounter.pop();
-    this.intervalColors.pop();
+    this.sliceColors.pop();
   }
 
   public fileSelected(image: any): void {
@@ -44,15 +51,15 @@ export class IntensitySliceComponent {
   }
 
   public intervalColorChange(value: string, index: number): void {
-    this.intervalColors[index] = value;
-    console.log(this.intervalColors);
+    this.sliceColors[index] = value;
+    console.log(this.sliceColors);
   }
 
   public submit(): void {
     this.loading = true;
-    this.processingService.intensitySlice(this.sliceValues, this.intervalColors, this.image)
+    this.processingService.intensitySlice(this.sliceValues, this.sliceColors, this.image)
       .subscribe((response: DIPResponse) => {
-        console.log(response);
+        this.result = response;
         this.loading = false;
       });
   }
